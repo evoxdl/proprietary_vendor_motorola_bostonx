2a8141dd55425640c2d6011760131196bec91d6e
Nick Reuter <nreuter85@gmail.com>
2026-07-14 16:36:33 -0500

    boston: Drop MotCamera3
    Change-Id: Idd2416f9a1259e9e7caee30fbd4db011d1c3f296

--- boston-vendor.mk
+++ boston-vendor.mk
@@ -3237,16 +3237,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/boston/proprietary/product/etc/CarrierSettings/s999480.pb:$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings/s999480.pb
 
 PRODUCT_COPY_FILES += \
-    vendor/motorola/boston/proprietary/product/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.camera3.xml \
-    vendor/motorola/boston/proprietary/product/etc/permissions/com.motorola.moto-uirefresh.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.moto-uirefresh.xml \
-    vendor/motorola/boston/proprietary/product/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml \
-    vendor/motorola/boston/proprietary/product/etc/permissions/privapp-permissions-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.motorola.camera3.xml \
-    vendor/motorola/boston/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml \
     vendor/motorola/boston/proprietary/recovery/root/vendor/firmware/goodix-csot-gt9916P-24030310-65E40FAF-boston.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/goodix-csot-gt9916P-24030310-65E40FAF-boston.bin \
     vendor/motorola/boston/proprietary/recovery/root/vendor/firmware/goodix-tm-gt9916P-24030313-65E40AA0-boston.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/goodix-tm-gt9916P-24030313-65E40AA0-boston.bin \
-    vendor/motorola/boston/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml \
-    vendor/motorola/boston/proprietary/system/etc/permissions/moto-core_services.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-core_services.xml \
-    vendor/motorola/boston/proprietary/system/etc/permissions/moto-settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-settings.xml \
     vendor/motorola/boston/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     vendor/motorola/boston/proprietary/vendor/etc/acdbdata/nn_ns_models/fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/nn_ns_models/fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd \
     vendor/motorola/boston/proprietary/vendor/etc/acdbdata/nn_ns_models/fai__2.3.0_0.1__3.0.0_0.0__eai_1.10.pmd:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/nn_ns_models/fai__2.3.0_0.1__3.0.0_0.0__eai_1.10.pmd \
@@ -3298,10 +3290,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/boston/proprietary/vendor/etc/jiigan/superiq_model_rear_uw:$(TARGET_COPY_OUT_VENDOR)/etc/jiigan/superiq_model_rear_uw \
     vendor/motorola/boston/proprietary/vendor/etc/libnfc-hal-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-hal-st.conf \
     vendor/motorola/boston/proprietary/vendor/etc/libnfc-nci-st.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci-st.conf \
-    vendor/motorola/boston/proprietary/vendor/etc/permissions/com.motorola.camera3.boston.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.boston.xml \
-    vendor/motorola/boston/proprietary/vendor/etc/permissions/com.motorola.camera3.content.ai.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.content.ai.xml \
-    vendor/motorola/boston/proprietary/vendor/etc/permissions/com.motorola.camera3.lens.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.lens.xml \
-    vendor/motorola/boston/proprietary/vendor/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.xml \
     vendor/motorola/boston/proprietary/vendor/etc/sensors/config/bmp5_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/bmp5_0.json \
     vendor/motorola/boston/proprietary/vendor/etc/sensors/config/icm4x6xx_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/icm4x6xx_0.json \
     vendor/motorola/boston/proprietary/vendor/etc/sensors/config/icp201xx_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/icp201xx_0.json \
@@ -3612,18 +3600,10 @@ PRODUCT_PACKAGES += \
     libvidhance \
     vendor.goodix.hardware.biometrics.fingerprint@2.1 \
     vendor.qti.hardware.camera.postproc@1.0-service-impl \
-    MotoSignatureApp \
-    MotCamera3AI \
     HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
-    MotCamera4 \
-    MotorolaSettingsProvider \
-    com.motorola.motosignature \
-    moto-core_services \
-    moto-settings \
     com.android.hotwordenrollment.common.util \
     capsense_reset \
     android.hardware.biometrics.fingerprint@2.1-goodixservice \
     android.hardware.biometrics.fingerprint@2.1-service-jv \
-    vendor.qti.camera.provider@2.7-service_64 \
-    motsettings
+    vendor.qti.camera.provider@2.7-service_64
